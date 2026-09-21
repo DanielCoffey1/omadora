f4c0bb731e6edf25cad4e7380568a33e0ef3f28a
Omadora contributors <contributors@omadora.invalid>
2026-09-21 01:54:49 -0400

    Include the browser needed to display the installer wizard

--- iso/build-installer.sh
+++ iso/build-installer.sh
@@ -5,12 +5,13 @@ mkdir -p /build /out/installer-base
 exec > >(tee /out/build-installer.log) 2>&1
 dnf install -y lorax squashfs-tools
 lorax --product Omadora --version 44 --release 44 --variant workstation \
-  --installpkgs anaconda-webui --volid OMADORA_44 --rootfs-type squashfs \
+  --installpkgs anaconda-webui --installpkgs firefox --volid OMADORA_44 --rootfs-type squashfs \
   --source https://download.fedoraproject.org/pub/fedora/linux/releases/44/Everything/x86_64/os/ \
   --source https://download.fedoraproject.org/pub/fedora/linux/updates/44/Everything/x86_64/ \
   --workdir /build/lorax-work --logfile /out/lorax.log /build/installer
 unsquashfs -ll /build/installer/images/install.img >/out/installer-files.txt
 grep -q 'usr/share/cockpit/anaconda-webui' /out/installer-files.txt
+grep -q 'usr/bin/firefox' /out/installer-files.txt
 cp /build/installer/images/boot.iso /out/installer-base/boot.iso
 cp /out/installer-files.txt /out/installer-base/
 (cd /out/installer-base && sha256sum boot.iso >SHA256SUMS)
